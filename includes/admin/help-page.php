8a42dc88a432289e85d479c8ee1d6e34579b00d9
inhumed-prog <luk.meyer@icloud.com>
2026-02-01 03:38:04 +0100

    Update help-page.php

--- includes/admin/help-page.php
+++ includes/admin/help-page.php
@@ -4,16 +4,16 @@
  */
 
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 /**
  * Render help page
  */
 function lm_monitor_help_page() {
-    // Security check
-    lm_monitor_verify_admin_access();
+	// Security check
+	lm_monitor_verify_admin_access();
 
-    // Simply include the view
-    include LM_MONITOR_PLUGIN_DIR . 'includes/admin/views/help-page-view.php';
+	// Simply include the view
+	include LM_MONITOR_PLUGIN_DIR . 'includes/admin/views/help-page-view.php';
 }
